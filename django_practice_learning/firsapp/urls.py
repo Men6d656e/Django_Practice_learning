da1ce0f6742dec94f303ad55a1b6d996e4ac7b0f
Men6d656e <men6d656e@gmail.com>
2025-01-11 00:01:24 +0500

    django learning on fire self learning

--- django_practice_learning/firsapp/urls.py
+++ django_practice_learning/firsapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('' , views.all , name='all'),
+    path('<int:chai_id>/' , views.chai_details , name='chai_details'),
 
 ]
--- django_practice_learning/firsapp/views.py
+++ django_practice_learning/firsapp/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
+
+
 
 # Create your views here.
 def all(request):
-    return render(request , 'firsapp/all.html')
+    chais = ChaiVarity.objects.all()
+    return render(request , 'firsapp/all.html' , { 'chais' : chais })
     # return HttpResponse("hello world, you are at all page")
+
+
+def chai_details(request , chai_id):
+    chai = get_object_or_404(ChaiVarity , pk = chai_id)
+    return render(request , 'firsapp/chai_details.html' , { 'chai' : chai })
